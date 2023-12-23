abec43f1e9f4b26142b2581257f2cb98a170e709
Vodina  Efem <efem.vodina@gmail.com>
2023-12-23 21:34:30 +0100

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 --- attributes id, email and name
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)
 );
