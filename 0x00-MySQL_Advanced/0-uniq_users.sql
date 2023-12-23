358f32c546e082094979b6d38d907a0a2b144939
Vodina  Efem <efem.vodina@gmail.com>
2023-12-23 21:25:21 +0100

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,1 +1,8 @@
+--- this command creates a table called users
+--- attributes id, email and name
 
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
+)
