efcc6c309c370ad99324c9c973472bb76228c723
Vodina Efem <efem.vodina@gmail.com>
2023-12-27 01:05:37 +0000

    Task 1 added

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ created to play with redis db
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -17,3 +17,19 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Callable = None):
+        if not self._redis.exists(key):
+            return None
+
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, key: str):
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str):
+        return self.get(key, fn=int)
