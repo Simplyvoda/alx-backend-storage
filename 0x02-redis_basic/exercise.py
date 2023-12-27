ea1d120c50e35be11d3834f3aa82463b82ecfaa8
Vodina Efem <efem.vodina@gmail.com>
2023-12-27 00:26:08 +0000

    Task 0 done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module contains a function
+created to play with redis db
+"""
+import redis
+import uuid
+from typing import Union
 
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
