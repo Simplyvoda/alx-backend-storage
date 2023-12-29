3e955e8c91d771f972f1c759fca2b7588d67aa78
Vodina  Efem <efem.vodina@gmail.com>
2023-12-29 20:40:40 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,43 +1,41 @@
 #!/usr/bin/env python3
-"""
-This module contains a function
-that returns HTML content of a url
-"""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
-from typing import Callable
 from functools import wraps
+from typing import Callable
 
 
-redis_instance = redis.Redis()
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-def counter(func: Callable) -> Callable:
-    """
-    decorator to store how many times
-    a url is accessed
-    """
-    @wraps(func)
-    def wrapper(*args) -> str:
-        """
-        wrapper function to store key
-        and value in redis
-        """
-        redis_instance.incr(f'count:{args[0]}')
-        result = redis_instance.get(f'result:{args[0]}')
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-        result = func(args[0])
-        redis_instance.set(f'count:{args[0]}', 0)
-        redis_instance.setex(f'result:{args[0]}', 10, result)
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
-    return wrapper
+    return invoker
 
 
-@counter
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    retrieves html content from
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
     a url
     """
     return requests.get(url).text
