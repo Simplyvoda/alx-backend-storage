1db034f9453a2c2f4c6e4631fded3f8eebdbfa25
Vodina Efem <efem.vodina@gmail.com>
2023-12-29 13:17:39 +0000

    Task 5 added again with corrections

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,14 +16,14 @@ def counter(func: Callable) -> Callable:
     a url is accessed
     """
     def wrapper(*args):
-        result = func(*args)
         key = f"count:{args[0]}"
         if redis_instance.exists(key):
             redis_instance.incr(key, 1)
+            redis_instance.expire(key, 10)
         else:
             redis_instance.set(key, 0, ex=10)
             redis_instance.incr(key, 1)
-        return result
+        return func(*args)
     return wrapper
 
 
@@ -36,6 +36,5 @@ def get_page(url: str) -> str:
     if url != "":
         response = requests.get(url)
         if response.status_code == 200:
-            html_content = response.text
-            return html_content
+            return response.text
     return "Failed to retrieve the page"
