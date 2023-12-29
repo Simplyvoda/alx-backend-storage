0cbd37744dc6222bc47697a0e39c9b3c225f8b17
Vodina Efem <efem.vodina@gmail.com>
2023-12-29 12:58:17 +0000

    added task 5

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+This module contains a function
+that returns HTML content of a url
+"""
+import redis
+import requests
+
+redis_instance = redis.Redis()
+
+
+def counter(func):
+    def wrapper(*args):
+        result = func(*args)
+        key = f"count:{args[0]}"
+        if redis_instance.exists(key):
+            redis_instance.incr(key, 1)
+        else:
+            redis_instance.set(key, 0, ex=10)
+            redis_instance.incr(key, 1)
+        return result
+    return wrapper
+                                                                                                
+
+@counter
+def get_page(url: str) -> str:
+    if url != "":
+        response = requests.get(url)
+        if response.status_code == 200:
+            html_content = response.text
+            return html_content
+    return "Failed to retrieve the page"
+
+test_response = get_page("http://slowwly.robertomurray.co.uk")
+print(test_response)
