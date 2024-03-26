60ec637ac62636968aab99d001ac2b1c5a4820e6
Simplyvoda <efem.vodina@gmail.com>
2024-03-26 13:10:02 -0700

    update: python script  that changes all topics of a school document based on the name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Task 10 module
+'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''changes all topics of a school document based on the name
+    '''
+    mongo_collection.update_many(
+            {'name': name},
+            {'$set': {'topics': topics}}
+    )
