7a614855ef9c6e833ec246e9bfa1a0867012bfe1
Simplyvoda <efem.vodina@gmail.com>
2024-03-27 19:07:57 +0000

    update: added store method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 Initiating Redis
 """
 import redis
+import uuid
 
 
 class Cache:
@@ -13,3 +14,11 @@ class Cache:
         '''
         self._redis = redis.Redis()
         self._redis.flushdb(True)
+
+
+    def store(data: Union[str, bytes, int, float]) -> str:
+        '''Stores a value in redis data storage and returns the key
+        '''
+        random_key = str(uuid.uuid4())
+        _redis.set(random_key, data)
+        return random_key
